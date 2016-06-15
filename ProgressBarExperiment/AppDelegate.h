72f37b67f85b394f309fdd9bff077fecc51159ec
Ken Murphy <kenmrph@gmail.com>
2016-06-14 18:32:37 -0700

    Initial Commit

---
+++ ProgressBarExperiment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgressBarExperiment
+//
+//  Created by Ken Murphy on 6/14/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgressBarExperiment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressBarExperiment
+//
+//  Created by Ken Murphy on 6/14/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
