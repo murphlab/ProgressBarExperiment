45f04759e26de7fa5f4ec02993588c92170043fd
Ken Murphy <kenmrph@gmail.com>
2016-06-16 16:04:41 -0700

    Controls for interactive debug/demo.

--- ProgressBarExperiment/ViewController.m
+++ ProgressBarExperiment/ViewController.m
@@ -15,35 +15,56 @@ static const int numDays = 29;
 
 @property (weak, nonatomic) IBOutlet ProgressBar *progressBar;
 
+@property (weak, nonatomic) IBOutlet UIStepper *maxStepper;
+@property (weak, nonatomic) IBOutlet UILabel *maxLabel;
+@property (weak, nonatomic) IBOutlet UIStepper *currentStepper;
+@property (weak, nonatomic) IBOutlet UILabel *currentLabel;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    self.progressBar.maxPosition = 1.0;
-    self.progressBar.position = 0.33;
+    self.progressBar.maxPosition = 15.0 / numDays;
     [self updateBubbleLabel];
+    [self updateSteppersFromProgressBar];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 - (IBAction)progressSliderChanged:(id)sender {
     NSLog(@"POSITION: %g", self.progressBar.position);
     [self updateBubbleLabel];
+    [self updateSteppersFromProgressBar];
 }
 
+- (IBAction)stepperTapped:(id)sender {
+    [self updateStepperLabels];
+    [self updateProgressBar];
+    [self updateBubbleLabel];
+}
+
+- (void)updateSteppersFromProgressBar
+{
+    self.maxStepper.value = (int)(self.progressBar.maxPosition * numDays);
+    self.currentStepper.value = (int)(self.progressBar.position * numDays);
+    [self updateStepperLabels];
+}
+
+- (void)updateStepperLabels
+{
+    self.maxLabel.text = [NSString stringWithFormat:@"%d", (int)self.maxStepper.value];
+    self.currentLabel.text = [NSString stringWithFormat:@"%d", (int)self.currentStepper.value];
+}
 - (void)updateBubbleLabel
 {
     int day = numDays * self.progressBar.position;
     self.progressBar.bubbleLabel.text = [NSString stringWithFormat:@"Day %d", day];
     NSLog(@"LABEL: %@", self.progressBar.bubbleLabel.text);
+}
+
+- (void)updateProgressBar
+{
+    self.progressBar.maxPosition = (CGFloat)self.maxStepper.value / (CGFloat)numDays;
+    self.progressBar.position = (CGFloat)self.currentStepper.value / (CGFloat)numDays;
 }
 
 @end
